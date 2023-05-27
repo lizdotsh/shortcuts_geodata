47804401b0fb49fa11319b2e3ca51c507c53b542
liz <lizjunkbox@gmail.com>
2023-05-27 12:13:51 -0400

    added parse logic

--- transform_geodata.R
+++ transform_geodata.R
@@ -20,15 +20,20 @@ test_convert_and_duration <- function() {
     )
     converted = covert_coords_to_stateid(state_test) 
     
+    
     print(converted)
+    
+    converted_google = covert_coords_to_stateid_google(state_test)
+    
+    print(converted_google)
     
     converted %>% 
         time_segments %>% 
         return()
 }
-test_convert_and_duration()
+#test_convert_and_duration()
 
-covert_coords_to_stateid <- function(coords_df, states = spData::us_states) {
+convert_coords_to_stateid <- function(coords_df, states = spData::us_states) {
     
     pts <- st_as_sf(coords_df, coords = c('lat', 'long'), crs = 4326) %>% st_transform(crs = 3857)
     states <- st_transform(states, crs = 3857)
@@ -60,10 +65,52 @@ covert_coords_to_stateid_google <- function(coords_df) {
 
 time_segments <- function(state_time_df) {
     state_time_df %>% 
-        filter(state != lag(state)) %>% 
+        filter(
+            state != lag(state) | (timestamp == max(timestamp) | timestamp == min(timestamp))
+            ) %>% 
         mutate(
-            end_unixtime = lead(unixtime), 
-            duration = end_unixtime - unixtime
-        ) %>% 
+            state = state,
+            start_datetime = timestamp,
+            end_datetime = lead(timestamp),
+            duration = lubridate::interval(start_datetime, end_datetime) %>% 
+                lubridate::as.duration(),
+            start_lat = lat,
+            start_long = long,
+            
+            .keep = 'none'
+        )  %>% 
+        na.omit() %>% 
         return()
 }
+
+read_location_log_file <- function(filename) {
+    data = readLines(filename)
+    location_lines <- data[str_detect(data, "Current Location")]
+    lat_long <- str_extract(location_lines, "-?\\d+\\.\\d+\\, -?\\d+\\.\\d+") %>% 
+        str_split(",", simplify = TRUE)
+    tibble(
+        timestamp = str_extract(location_lines, "\\[.*\\]") %>% gsub("\\[|\\]|shqiphone.shortcuts|Current Location:", "", .),
+        lat = lat_long[,2],
+        long = lat_long[,1]
+        )%>% 
+        mutate(
+            timestamp = lubridate::mdy_hms(timestamp, tz = 'America/New_York')
+        ) %>%
+        arrange(timestamp) %>% 
+        return()
+}
+
+parse_file <- function(filename) {
+    location_history = read_location_log_file(filename) 
+    parsed_location_history = convert_coords_to_stateid(location_history, states = spData::us_states)
+    write_csv(parsed_location_history, 'data/parsed_location_history.csv')
+    saveRDS(parsed_location_history, file = 'data/parsed_location_history_serialized.rds')
+    location_intervals = time_segments(parsed_location_history)
+    write_csv(location_intervals, 'data/location_intervals.csv')
+    saveRDS(location_intervals, file = 'data/location_intervals_serialized.rds')
+    return(location_intervals)
+    
+}
+
+parse_file('data/scratch.md')
+
